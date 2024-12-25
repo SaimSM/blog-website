caba9a09f02236faab33ed7088f7c46e42134a5d
SaimSM <saimdilse2004@gmail.com>
2024-12-25 15:51:04 +0500

    build error resolved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-purple-50 to-purple-100">
@@ -10,12 +11,12 @@ export default function Home() {
           Explore the blog to learn and grow!
         </p>
         <div className="mt-8">
-          <a
+          <Link
             href="/Blog"
             className="bg-purple-600 text-white px-6 py-3 rounded-lg shadow-md hover:bg-purple-700"
           >
             Go to Blog
-          </a>
+          </Link>
         </div>
       </div>
     </div>
